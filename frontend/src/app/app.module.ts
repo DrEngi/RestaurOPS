6d368e09ade3028ed976242f1daeace8e50ad647
Tom <87573030+TomReilly1@users.noreply.github.com>
2022-03-27 13:29:39 -0400

    Revert "Testing"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +24,6 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     MatCardModule,
     MatButtonModule
   ],
--- frontend/src/app/components/customer/item/item.component.ts
+++ frontend/src/app/components/customer/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,11 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  
-  @Input() image = '';
-  @Input() name = '';
-  @Input() price = -1;
-  
 
   constructor() { }
 
